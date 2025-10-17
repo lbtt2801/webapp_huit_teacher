2f429f2aea3872f75b012f8968279f9dffe7c882
Truong.Le <lebuitantruong@gmail.com>
2025-10-17 11:54:11 +0700

    fix api in detail feedback

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "4ab3cb498dfe4b44b566ab31aae7761d",
+const RESOURCES = {"flutter_bootstrap.js": "a50835c926202610b22f844115f9c61b",
 "version.json": "eb139fbf9d40832aaba52375cebf9ca6",
 "index.html": "7b4296f3fba6d9fe24de71752f84af2c",
 "/": "7b4296f3fba6d9fe24de71752f84af2c",
 "js/js_library.js": "bffbcddb5d14265428dd3ffa8ca31e7a",
-"main.dart.js": "112d62eed87b648ac21f66e9c8c1e89f",
+"main.dart.js": "011cfeaf2f28031d46d892d1e1e00145",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
